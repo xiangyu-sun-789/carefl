9d64eb63502ea60fc32a48c069b0a5f4256a65d1
shawnxys789 <shawnxys789@outlook.com>
2022-05-22 15:14:47 -0700

    remove code that's not relevant to causal discovery

--- models/my_carefl.py
+++ models/my_carefl.py
@@ -47,8 +47,6 @@ class MY_CAREFL:
         # initial guess on correct model to be updated after each fit
         self.dim = None
         self.direction = 'none'
-        self.flow_xy = self.flow_yx = self.flow = None
-        self._nhxy = self._nhyx = self._nlxy = self._nlyx = None
 
     def flow_lr(self, data, return_scores=False):
         """
@@ -78,103 +76,24 @@ class MY_CAREFL:
         """
         dset, test_dset, dim = self._get_datasets(data)
         self.dim = dim
+
         # Conditional Flow Model: X->Y
         torch.manual_seed(self.config.training.seed)
         flows_xy, _ = self._train(dset)
-        self.flow_xy, score_xy, self._nlxy, self._nhxy = self._evaluate(flows_xy, test_dset)
+        _, score_xy, _, _ = self._evaluate(flows_xy, test_dset)
+
         # Conditional Flow Model: Y->X
         torch.manual_seed(self.config.training.seed)
         flows_yx, _ = self._train(dset, parity=True)
-        self.flow_yx, score_yx, self._nlyx, self._nhyx = self._evaluate(flows_yx, test_dset, parity=True)
+        _, score_yx, _, _ = self._evaluate(flows_yx, test_dset, parity=True)
+
         # compute LR
         p = score_xy - score_yx
-        self._update_dir(p)
+        self._update_direction(p)
         if return_scores:
             return p, score_xy, score_yx
         else:
             return p
-
-    def predict_proba(self, data, return_scores=False):
-        """
-        Prediction method for pairwise causal inference using the Affine Flow LR model.
-
-        A wrapper around `flow_lr` to have a consistent signature with the other implementations (of ANM, RECI, etc..)
-        """
-        if return_scores:
-            p, sxy, syx = self.flow_lr(data, return_scores=return_scores)
-            return p, self.direction, sxy, syx
-        else:
-            p = self.flow_lr(data, return_scores=return_scores)
-            return p, self.direction
-
-    def fit_to_sem(self, data, dag=None, return_scores=False):
-        """
-        Assuming data columns follow the causal ordering, we fit the associated SEM.
-
-        Parameters:
-        ----------
-        data: numpy.ndarray
-            A dataset whose columns are ordered following the causal ordering \pi. The causal ordering
-            can be a result of causal discovery algorithm, or expert judgement.
-        dag: None
-            Not in use. Only purpose is to have the same method signature as the other algorithms.
-        return_scores: bool
-            If True, return the scors of all the different flows tested when fitting, else return None
-        """
-        dset, test_dset, dim = self._get_datasets(data)
-        self.dim = dim
-        torch.manual_seed(self.config.training.seed)
-        flows, _ = self._train(dset)
-        self.flow, score, self._nlxy, self._nhxy = self._evaluate(flows, test_dset)
-        return score if return_scores else None
-
-    def predict_intervention(self, x0_val, n_samples=100, iidx=0):
-        """
-        We predict the value of x given an intervention do(x_iidx = x0_val)
-
-        As explained in Appendix E.1, we do not need to invert the flow to perform interventions,
-        but doing so provides a simpler and more efficient algorithm. Thus, this implementation
-        will follow Algorithm 2 of Appendix E.1.
-
-        This proceeds in 3 steps:
-         1) invert flow to find corresponding entry for z_iidx at x_iidx=x0_val
-         2) sample z from prior (number of samples is n_samples), and replace z_iidx by inferred value from strep 1
-         3) propagate z through flow to get samples for x | do(x_iidx=x0_val)
-        """
-        # invert flow to infer value of latent corresponding to interventional variable
-        x_int = np.zeros((1, self.dim))
-        x_int[0, iidx] = x0_val
-        z_int = self._forward_flow(x_int)[0, iidx]
-        # sample from prior and ensure z_intervention_index = z_int
-        z = self.flow.prior.sample((n_samples,)).cpu().detach().numpy()
-        z_est = np.zeros((1, self.dim))
-        z[:, iidx] = z_est[:, iidx] = z_int
-        # propagate the latent sample through flow
-        x = self._backward_flow(z)
-        x_from_z_est = self._backward_flow(z_est)  # to compare to x when expectation is taken after pass through flow
-        # sanity check: check x_intervention_index == x0_val
-        assert (np.abs(x[:, iidx] - x0_val) < 1e-3).all()
-        return x.mean(0).reshape((1, self.dim)), x_from_z_est
-
-    def predict_counterfactual(self, x_obs, cf_val, iidx=0):
-        """
-        Given observation x_obs we estimate the counterfactual of setting x_obs[intervention_index] = cf_val
-
-        This proceeds in 3 steps:
-         1) abduction - pass-forward through flow to infer latents for x_obs
-         2) action - pass-forward again for latent associated with cf_val
-         3) prediction - backward pass through the flow
-        """
-        # abduction:
-        z_obs = self._forward_flow(x_obs)
-        # action (get latent variable value under counterfactual)
-        x_cf = np.copy(x_obs)
-        x_cf[0, iidx] = cf_val
-        z_cf_val = self._forward_flow(x_cf)[0, iidx]
-        z_obs[0, iidx] = z_cf_val
-        # prediction (pass through the flow):
-        x_post_cf = self._backward_flow(z_obs)
-        return x_post_cf
 
     def _get_optimizer(self, parameters):
         """
@@ -274,7 +193,7 @@ class MY_CAREFL:
             all_loss_vals.append(loss_vals)
         return flows, all_loss_vals
 
-    def _get_params_from_idx(self, idx):
+    def _get_params_from_idx(self, idx):  # for debug
         return self.n_layers[idx // len(self.n_hidden)], self.n_hidden[idx % len(self.n_hidden)]
 
     def _evaluate(self, flows, test_dset, parity=False):
@@ -328,16 +247,5 @@ class MY_CAREFL:
             dim = input[0][0].shape[-1]
             return input[0], input[1], dim
 
-    def _update_dir(self, p):
-        self.flow = self.flow_xy if p >= 0 else self.flow_yx
+    def _update_direction(self, p):
         self.direction = 'x->y' if p >= 0 else 'y->x'
-
-    def _forward_flow(self, data):
-        if self.flow is None:
-            raise ValueError('Model needs to be fitted first')
-        return self.flow.forward(torch.tensor(data.astype(np.float32)).to(self.device))[0][-1].detach().cpu().numpy()
-
-    def _backward_flow(self, latent):
-        if self.flow is None:
-            raise ValueError('Model needs to be fitted first')
-        return self.flow.backward(torch.tensor(latent.astype(np.float32)).to(self.device))[0][-1].detach().cpu().numpy()
